fa0b4edefa2a3aaf8fc9adab2e65da63b66fb241
Manav Patel <2001045@y115.org>
2018-03-09 09:40:20 -0600

    Added player look towards mouse

---
+++ Assets/Scripts/Player_Scripts/Player_RotateToMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_RotateToMouse : MonoBehaviour {
+
+    float speed = 10f;
+	
+	// Update is called once per frame
+	private void Update () {
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x,mousePosition.y - transform.position.y);
+
+        transform.up = direction;
+        
+
+
+
+	}
+}
