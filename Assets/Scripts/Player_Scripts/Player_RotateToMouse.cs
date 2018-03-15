6d0081822fe78bd149ab2ad682454f51ee7f5c5e
Manav Patel <2001045@y115.org>
2018-03-15 09:51:29 -0500

    Fixed rotation bug and added charge shot

--- Assets/Scripts/Player_Scripts/Player_RotateToMouse.cs
+++ Assets/Scripts/Player_Scripts/Player_RotateToMouse.cs
@@ -10,7 +10,7 @@ public class Player_RotateToMouse : MonoBehaviour {
     void Update()
     {
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation,1);
 
--- Assets/Scripts/Player_Scripts/Player_Shooting.cs
+++ Assets/Scripts/Player_Scripts/Player_Shooting.cs
@@ -9,6 +9,15 @@ public class Player_Shooting : MonoBehaviour {
     public float spread;
     public bool canShoot;
     public float waitTime = 0.1f;
+    public Transform BulletSpawner;
+    [SerializeField]
+    private float charge;
+    public float chargeAdder;
+    [SerializeField]
+    private float timer;
+    [SerializeField]
+    private float MaxTimer = 1f;
+
 	// Use this for initialization
 	void Start () {
         velocity =  Random.Range(500f, 1000f);
@@ -20,23 +29,50 @@ public class Player_Shooting : MonoBehaviour {
 	void Update () {
 
 
-        if (Input.GetMouseButton(0) && canShoot)
+        if (Input.GetMouseButton(1))// && canShoot && !Input.GetMouseButton(0))
         {
+            charge += 0.1f;
+
+            if (charge >= MaxTimer)
+                charge = MaxTimer;
+
+            if(charge >= MaxTimer)
+            {
+                Debug.Log("Charged");
+            }
 
 
-            StartCoroutine(MakeBullet());
 
+
+        }
+
+        if (Input.GetMouseButtonUp(1) && charge >= MaxTimer)
+        {
+            charge = 0f;
+            Debug.Log("Fired");
+            StartCoroutine(MakeBullet(new Vector3(20, 20, 1)));
+        }else if(Input.GetMouseButtonUp(1) && charge < MaxTimer)
+        {
+            charge = 0f;
+            
+        }
+        
+
+        else if(Input.GetMouseButton(0) && canShoot && !Input.GetMouseButton(1))
+        {
+            StartCoroutine(MakeBullet(new Vector3(5,5,1)));
         }
 	}
 
-    public IEnumerator MakeBullet()
+    public IEnumerator MakeBullet(Vector3 scale)
     {
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         direction.Normalize();
         GameObject CBullet = (GameObject)Instantiate(bullet, transform.position, rotation);
-        CBullet.transform.localPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + 20);
+        CBullet.transform.localPosition = BulletSpawner.transform.position;
+        CBullet.transform.localScale = scale;
         CBullet.GetComponent<Rigidbody2D>().velocity = direction * velocity;
         canShoot = false;
         yield return new WaitForSeconds(waitTime);
