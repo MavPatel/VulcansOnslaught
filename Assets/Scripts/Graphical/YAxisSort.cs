446b0b265b00a0121df1c8e7868868741394f2cd
Manav Patel <2001045@y115.org>
2018-03-22 12:46:11 -0500

    Added yaxissort and adjusted layers

---
+++ Assets/Scripts/Graphical/YAxisSort.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YAxisSort : MonoBehaviour {
+
+    private SpriteRenderer sprite;
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	
+	void LateUpdate () {
+        sprite.sortingOrder = (int)transform.position.y;
+	}
+}
