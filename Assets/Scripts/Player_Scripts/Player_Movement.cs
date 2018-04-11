5dc29f440139e799eaa4b569be07449be9bb5dd4
Manav Patel <37078220+MavPatel@users.noreply.github.com>
2018-04-11 09:35:33 -0500

    FixedAnimations added head Movement

--- Assets/Scripts/Player_Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Scripts/Player_Movement.cs
@@ -9,6 +9,8 @@ public class Player_Movement : MonoBehaviour
     public float velocity;
     Vector2 movement;
     private Rigidbody2D rigid;
+    private Animator anim;
+    public Animator animHead;
     private SpriteRenderer sprRend;
     public SpriteRenderer feetRend;
 
@@ -21,11 +23,13 @@ public class Player_Movement : MonoBehaviour
        
         rigid = GetComponent<Rigidbody2D>();
         sprRend = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
     void FixedUpdate()
     {
        
         Movement();
+        MouseAnim();
     }
 
 
@@ -48,33 +52,37 @@ public class Player_Movement : MonoBehaviour
         movement = new Vector2(inputX, inputY);
         rigid.velocity = movement.normalized * velocity;
 
-        if (Input.GetKey(KeyCode.S))
+        if (inputX > 0 || inputX < 0 || inputY > 0 || inputY < 0)
         {
-            down = true;
-            up = false;
-            left = false;
-            right = false;
-        }else if (Input.GetKey(KeyCode.W))
-        {
-            up = true;
-            down = false;
-            left = false;
-            right = false;
-        }else if (Input.GetKey(KeyCode.A))
-        {
-            
-            left = true;
-            right = true;
-            up = false;
-            down = false;
-        }else if (Input.GetKey(KeyCode.D))
-        {
-            
-            right = true;
-            down = false;
-            up = false;
-            left = false;
+            anim.SetFloat("x", inputX);
+            anim.SetFloat("y", inputY);
         }
+     
+
+        if(velocity > 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        
+       
+    }
+
+    void MouseAnim()
+    {
+       
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+
+
+        
+        animHead.SetFloat("x", Mathf.FloorToInt(angle / 90));
+        animHead.SetFloat("y", Mathf.FloorToInt(angle / 90));
+
     }
 
  
