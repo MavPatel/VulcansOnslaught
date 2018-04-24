7c92e97d89a734dd90ddade9cb597d8a535a2d29
Jonah68 <37078282+Jonah68@users.noreply.github.com>
2018-04-24 09:12:01 -0500

    k

---
+++ Assets/Scripts/Levelmanager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levelmanager : MonoBehaviour {
+
+    public void LoadLevel(string level)
+    {
+        Debug.Log("Attempting to load " + level);
+    }
+}
--- Assets/Scripts/Player_Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Scripts/Player_Movement.cs
@@ -61,16 +61,8 @@ public class Player_Movement : MonoBehaviour
 
         if(velocity > 0)
         {
-<<<<<<< HEAD
-            sprRend.flipX = false;
-            feetRend.flipX = false;
-            right = true;
-            down = false;
-            up = false;
-            left = false;
-=======
+
             anim.SetBool("isWalking", true);
->>>>>>> Manav
         }
         else
         {
