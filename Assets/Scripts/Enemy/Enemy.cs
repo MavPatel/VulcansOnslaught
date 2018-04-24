fbaf031e4fbb1be28e90ee3771e5242de9d03d9c
Manav Patel <37078220+MavPatel@users.noreply.github.com>
2018-04-24 12:50:29 -0500

    asdf

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    
     public int health;
     public  int attackSpeed;
     public int damage;
     public int moveSpeed;
     public int movementNumber = 0;
+    public bool triggered = false;
+    private Rigidbody2D rigid;
+
+
+    public void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
 
 
     public void ChangeHealth(int ammount)
@@ -22,14 +31,29 @@ public class Enemy : MonoBehaviour {
        
     }
 
+    public void Trigger(GameObject target)
+    {
+        triggered = true;
+        
+        transform.position += (target.transform.position - transform.position).normalized * moveSpeed * Time.deltaTime;
+    }
+
+    public void Chill()
+    {
+        triggered = false;
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
 
         if (collision.gameObject.tag == "Bullet")
         {
+            
             ChangeHealth(collision.gameObject.GetComponent<Bullet>().damage);
             Debug.Log(health);
         }
     }
+
+    
 
 }
---
+++ Assets/Scripts/Enemy/TriggerEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnemy : MonoBehaviour {
+
+    private Skeleton skeleton;
+	
+	void Start () {
+        skeleton = GetComponentInParent<Skeleton>();
+        
+	}
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            
+            skeleton.Trigger(collision.gameObject);
+        }else if (skeleton.triggered)
+        {
+            skeleton.Trigger(collision.gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            skeleton.Chill();
+        }
+    }
+}
