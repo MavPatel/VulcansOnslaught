2cbe5dcb45e1594a506a2c2925414da027a29969
Jonah68 <37078282+Jonah68@users.noreply.github.com>
2018-04-30 10:00:55 -0500

    k

---
+++ Assets/Scripts/DontdestroyOnLoad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontdestroyOnLoad : MonoBehaviour {
+ 
+ public class MusicClass : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+            GameObject.FindGameObjectWithTag("Music").GetComponent<MusicClass>().PlayMusic();
+            if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
+
+}
