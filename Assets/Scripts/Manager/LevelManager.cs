5193b7f8da26bfe8872b7abd19795f160118310d
Manav Patel <37078220+MavPatel@users.noreply.github.com>
2018-05-14 09:53:34 -0500

    asdf

--- Assets/Scripts/Manager/LevelManager.cs
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -5,23 +5,11 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
 
-    public static LevelManager Instance { get; private set; }
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
     
     public void ChangeLevel(string level)
     {
-        SceneManager.LoadSceneAsync(level); 
+        SceneManager.LoadScene(level); 
     }
+   
 }
--- Assets/SwitchLevel.cs
+++ Assets/SwitchLevel.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SwitchLevel : MonoBehaviour {
 
 
 
-
+    LevelManager level;
+    private void Start()
+    {
+        level = GameObject.FindGameObjectWithTag("Level").GetComponent<LevelManager>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && SceneManager.GetActiveScene().name == "Level-1")
         {
-            LevelManager.Instance.ChangeLevel("Level-02");
+            level.ChangeLevel("Level-2");
+        }else if(collision.gameObject.tag == "Player" && SceneManager.GetActiveScene().name == "Level-2")
+        {
+            level.ChangeLevel("Level-3");
+        }
+        else if (collision.gameObject.tag == "Player" && SceneManager.GetActiveScene().name == "Level-3")
+        {
+            level.ChangeLevel("Level-4");
         }
     }
 }
